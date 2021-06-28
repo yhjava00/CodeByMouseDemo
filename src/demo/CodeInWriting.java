04194f2fe8e12bfd865d6697e87e9f9509fc7342
yhjava00 <yhjava00@gmail.com>
2021-06-28 17:47:01 +0900

    init commit

--- src/demo/CodeInWriting.java
+++ src/demo/CodeInWriting.java
@@ -10,8 +10,10 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.gson.Gson;
 
@@ -25,6 +27,10 @@ public class CodeInWriting {
             this.type = type;
             this.count = count;
         }
+        @Override
+        public String toString() {
+        	return text;
+        }
     }
     static public class NameEntity {
         final String text;
@@ -35,30 +41,30 @@ public class CodeInWriting {
             this.type = type;
             this.count = count;
         }
+        @Override
+        public String toString() {
+        	return text;
+        }
     }
  
-    static public void main(String[] args) {
+    public static void main(String[] args) {
 
         String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken";         
         String accessKey = "";   // 발급받은 API Key
         String analysisCode = "ner";        // 언어 분석 코드
         
-        String text = "헬로 월드를 출력하세요";           // 분석할 텍스트 데이터
+        
+        StringBuilder text = new StringBuilder();
+        text.append("문장 헬로 월드를 출력하시오");           // 분석할 텍스트 데이터
         
         Gson gson = new Gson();
          
-
-//        text += "윤동주(尹東柱, 1917년 12월 30일 ~ 1945년 2월 16일)는 한국의 독립운동가, 시인, 작가이다." +
-//                "중국 만저우 지방 지린 성 연변 용정에서 출생하여 명동학교에서 수학하였고, 숭실중학교와 연희전문학교를 졸업하였다. 숭실중학교 때 처음 시를 발표하였고, 1939년 연희전문 2학년 재학 중 소년(少年) 지에 시를 발표하며 정식으로 문단에 데뷔했다." +
-//                "일본 유학 후 도시샤 대학 재학 중 , 1943년 항일운동을 했다는 혐의로 일본 경찰에 체포되어 후쿠오카 형무소(福岡刑務所)에 투옥, 100여 편의 시를 남기고 27세의 나이에 옥중에서 요절하였다. 사인이 일본의 생체실험이라는 견해가 있고 그의 사후 일본군에 의한 마루타, 생체실험설이 제기되었으나 불확실하다. 사후에 그의 시집 《하늘과 바람과 별과 시》가 출간되었다." +
-//                "일제 강점기 후반의 양심적 지식인으로 인정받았으며, 그의 시는 일제와 조선총독부에 대한 비판과 자아성찰 등을 소재로 하였다. 그의 친구이자 사촌인 송몽규 역시 독립운동에 가담하려다가 체포되어 일제의 생체 실험으로 의문의 죽음을 맞는다. 1990년대 후반 이후 그의 창씨개명 '히라누마'가 알려져 논란이 일기도 했다. 본명 외에 윤동주(尹童柱), 윤주(尹柱)라는 필명도 사용하였다.";
-
-         
+        
         Map<String, Object> request = new HashMap<>();
         Map<String, String> argument = new HashMap<>();
  
         argument.put("analysis_code", analysisCode);
-        argument.put("text", text);
+        argument.put("text", text.toString());
  
         request.put("access_key", accessKey);
         request.put("argument", argument);
@@ -92,9 +98,6 @@ public class CodeInWriting {
             }
             responBodyJson = sb.toString();
             
-            System.out.println(responBodyJson);
-            System.out.println("--------------------");
-            
             // http 요청 오류 시 처리
             if ( responseCode != 200 ) {
                 // 오류 내용 출력
@@ -118,90 +121,53 @@ public class CodeInWriting {
             returnObject = (Map<String, Object>) responeBody.get("return_object");
             sentences = (List<Map>) returnObject.get("sentence");
  
-            Map<String, Morpheme> morphemesMap = new HashMap<String, Morpheme>();
-            Map<String, NameEntity> nameEntitiesMap = new HashMap<String, NameEntity>();
-            List<Morpheme> morphemes = null;
-            List<NameEntity> nameEntities = null;
- 
+            List<String> keyWords = new ArrayList<>();
+            
+            Set<String> keyWord = new HashSet<>();
+            
+            keyWord.add("출력");
+            
             for( Map<String, Object> sentence : sentences ) {
                 // 형태소 분석기 결과 수집 및 정렬
                 List<Map<String, Object>> morphologicalAnalysisResult = (List<Map<String, Object>>) sentence.get("morp");
                 for( Map<String, Object> morphemeInfo : morphologicalAnalysisResult ) {
                     String lemma = (String) morphemeInfo.get("lemma");
-                    Morpheme morpheme = morphemesMap.get(lemma);
-                    if ( morpheme == null ) {
-                        morpheme = new Morpheme(lemma, (String) morphemeInfo.get("type"), 1);
-                        morphemesMap.put(lemma, morpheme);
-                    } else {
-                        morpheme.count = morpheme.count + 1;
-                    }
-                }
- 
-                // 개체명 분석 결과 수집 및 정렬
-                List<Map<String, Object>> nameEntityRecognitionResult = (List<Map<String, Object>>) sentence.get("NE");
-                for( Map<String, Object> nameEntityInfo : nameEntityRecognitionResult ) {
-                    String name = (String) nameEntityInfo.get("text");
-                    NameEntity nameEntity = nameEntitiesMap.get(name);
-                    if ( nameEntity == null ) {
-                        nameEntity = new NameEntity(name, (String) nameEntityInfo.get("type"), 1);
-                        nameEntitiesMap.put(name, nameEntity);
-                    } else {
-                        nameEntity.count = nameEntity.count + 1;
-                    }
+                    if(keyWord.contains(lemma))
+                    	keyWords.add(lemma);
                 }
             }
- 
-            if ( 0 < morphemesMap.size() ) {
-                morphemes = new ArrayList<Morpheme>(morphemesMap.values());
-                morphemes.sort( (morpheme1, morpheme2) -> {
-                    return morpheme2.count - morpheme1.count;
-                });
+            
+            StringBuilder code = new StringBuilder();
+            
+            code.append("public class Main {").append("\n");
+            code.append("\tpublic static void main(String[] args) {").append("\n");
+            
+            int tapLev = 2;
+            
+            for(int i=0; i<keyWords.size(); i++) {
+            	switch (keyWords.get(i)) {
+				case "출력":
+					String allWord = text.toString();
+					String printWord = allWord.substring(allWord.indexOf("문장 ")+3);
+					printWord = printWord.substring(0, printWord.indexOf("를"));
+					for(int j=0; j<tapLev; j++) {
+						code.append("\t");
+					}
+					code.append("System.out.println(\"").append(printWord).append("\");\n");
+					break;
+				}
             }
- 
-            if ( 0 < nameEntitiesMap.size() ) {
-                nameEntities = new ArrayList<NameEntity>(nameEntitiesMap.values());
-                nameEntities.sort( (nameEntity1, nameEntity2) -> {
-                    return nameEntity2.count - nameEntity1.count;
-                });
-            }
- 
-            // 형태소들 중 명사들에 대해서 많이 노출된 순으로 출력 ( 최대 5개 )
-            morphemes
-                .stream()
-                .filter(morpheme -> {
-                    return morpheme.type.equals("NNG") ||
-                            morpheme.type.equals("NNP") ||
-                            morpheme.type.equals("NNB");
-                })
-                .limit(5)
-                .forEach(morpheme -> {
-                    System.out.println("[명사] " + morpheme.text + " ("+morpheme.count+")" );
-                });
- 
-            // 형태소들 중 동사들에 대해서 많이 노출된 순으로 출력 ( 최대 5개 )
-            System.out.println("");
-            morphemes
-                .stream()
-                .filter(morpheme -> {
-                    return morpheme.type.equals("VV");
-                })
-                .limit(5)
-                .forEach(morpheme -> {
-                    System.out.println("[동사] " + morpheme.text + " ("+morpheme.count+")" );
-                });
- 
-            // 인식된 개채명들 많이 노출된 순으로 출력 ( 최대 5개 )
-            System.out.println("");
-            nameEntities
-                .stream()
-                .limit(5)
-                .forEach(nameEntity -> {
-                    System.out.println("[개체명] " + nameEntity.text + " ("+nameEntity.count+")" );
-                });
+            
+            code.append("\t}").append("\n");
+            code.append("}");
+            
+            System.out.println(code.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    
 }
---
+++ src/test/Main.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("헬로 월드");
+	}
+}
